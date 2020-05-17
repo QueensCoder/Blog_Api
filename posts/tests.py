5a49afc08abcd0fa791df208e674d274ae936625
Ozal Khan <ozalkhan196@gmail.com>
2020-05-17 13:57:31 -0400

    combinded all  tests into on method to handle all of the blog content

--- posts/tests.py
+++ posts/tests.py
@@ -9,18 +9,27 @@ class PostModelTest(TestCase):
     def setUpTestData(cls):
         user = User.objects.create_user(
             'john', 'lennon@thebeatles.com', 'johnpassword')
+        user.save()
         post = Post.objects.create(
             title='first title', body='test body', author=user)
+        post.save()
 
-    def test_title(self):
+    def test_blog_content(self):
         post = Post.objects.get(id=1)
         self.assertEquals(f'{post.title}', 'first title')
-
-    def test_author(self):
-        post = Post.objects.get(id=1)
         self.assertEquals(f'{post.author.username}', 'john')
         self.assertEquals(f'{post.author.email}', 'lennon@thebeatles.com')
-
-    def test_body(self):
-        post = Post.objects.get(id=1)
         self.assertEquals(f'{post.body}', 'test body')
+
+    # def test_title(self):
+    #     post = Post.objects.get(id=1)
+    #     self.assertEquals(f'{post.title}', 'first title')
+
+    # def test_author(self):
+    #     post = Post.objects.get(id=1)
+    #     self.assertEquals(f'{post.author.username}', 'john')
+    #     self.assertEquals(f'{post.author.email}', 'lennon@thebeatles.com')
+
+    # def test_body(self):
+    #     post = Post.objects.get(id=1)
+    #     self.assertEquals(f'{post.body}', 'test body')
