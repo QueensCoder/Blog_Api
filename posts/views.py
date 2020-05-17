a2cf7203cf225019dd9c375f2e2918cc761555ff
Ozal Khan <ozalkhan196@gmail.com>
2020-05-17 15:47:23 -0400

    fixed issue with generics by using model serialer instead of regular serialzer

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permissions_classes = [
+        permissions.AllowAny
+    ]
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permissions_classes = [
+        permissions.AllowAny
+    ]
