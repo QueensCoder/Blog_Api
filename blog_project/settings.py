e669dbb036f567c045aefb54cfc97aa0fcabdd1b
Ozal Khan <ozalkhan196@gmail.com>
2020-05-27 14:52:26 -0400

    added setting for django heroku

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -113,3 +114,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
