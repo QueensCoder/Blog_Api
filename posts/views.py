0abbd8bc88a31587bbd49eddfe6d349916a57dd3
Ozal Khan <ozalkhan196@gmail.com>
2020-06-04 18:41:34 -0400

    changed generics to viewsets and url patterns to use router.urls for cleaner implementation

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,11 @@
-from rest_framework import generics, permissions, viewsets
+from rest_framework import permissions, viewsets
 from .models import Post
 from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 from django.contrib.auth import get_user_model
-# is authenticated allows users to perform actions if authenticated
-from rest_framework.permissions import IsAuthenticated
+
+
+# viewsets will handle list and detail views
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -13,26 +14,6 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
 
-class PostList(generics.ListCreateAPIView):
-    # view level permissions
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-# if not author post is now read only using permission specified
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    # view level permissions
-    permission_classes = (IsAuthorOrReadOnly,)
-    # is author or read only does not let test user update post
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class UserList(generics.ListCreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
